e78966604f8badb00004fe976af0edbe9a92993b
Its-Tejaswi <tejaswisidhanthi@gmail.com>
2020-11-15 09:39:09 +0530

    captured Image

--- src/pages/PageCamara.vue
+++ src/pages/PageCamara.vue
@@ -1,10 +1,22 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video ref="video" class="full-width" autoplay />
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="200"
+      />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn size="lg" round color="grey-10" icon="eva-camera" />
+      <q-btn
+        @click="captureImage"
+        size="lg"
+        round
+        color="grey-10"
+        icon="eva-camera"
+      />
     </div>
     <div class="row justify-center q-ma-md">
       <q-input
@@ -40,15 +52,27 @@ export default {
         photo: null,
         date: Date.now(),
       },
+      imageCaptured: false,
     };
   },
   methods: {
     initCamera() {
-      navigator.mediaDevices.getUserMedia({
+      navigator.mediaDevices
+        .getUserMedia({
           video: true,
-        }).then(stream => {
+        })
+        .then((stream) => {
           this.$refs.video.srcObject = stream;
         });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
     },
   },
   mounted() {
